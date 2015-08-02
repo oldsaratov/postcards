6a4c0b00a715da075d2ea5286a43c30e66c8a866
okolobaxa <okolobaxa@gmail.com>
2015-08-02 21:51:20 +0300

    update text on front page

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using System;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -13,6 +14,28 @@ namespace PostcardsManager
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            i18n.UrlLocalizer.IncomingUrlFilters += delegate(Uri url)
+            {
+                if (url.LocalPath.Contains("/Content/"))
+                {
+                    return false;
+                }
+                return true;
+            };
+            i18n.UrlLocalizer.OutgoingUrlFilters += delegate(string url, Uri currentRequestUrl)
+            {
+                Uri uri;
+                if (Uri.TryCreate(url, UriKind.Absolute, out uri)
+                    || Uri.TryCreate(currentRequestUrl, url, out uri))
+                {
+                    if (uri.LocalPath.Contains("/Content/"))
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            };
         }
     }
 }
